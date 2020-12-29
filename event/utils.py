9418cb92c6de2bbc812631dafbb247560b0a0b33
goodnighthy <hengyu1213@163.com>
2020-12-29 12:01:13 +0800

    add filter Q

--- event/utils.py
+++ event/utils.py
@@ -7,8 +7,26 @@ from account.models import User
 from core.consts import EVENT_TRANSFORM_ACTIVATE, EVENT_TRANSFORM_PAY, EVENT_TRANSFORM_TWICE
 from event.models import ClickEvent, TransformEvent
 
+def transform_blank_to_zero(user: User):
+    if user.android_id == '':
+        android_id = '0'
+    else:
+        android_id = user.android_id
+    if user.imei == '':
+        imei = '0'
+    else:
+        imei = user.imei
+    if user.oaid == '':
+        oaid = '0'
+    else:
+        oaid = user.oaid
+    if user.mac == '':
+        mac = '0'
+    else:
+        mac = user.mac
+    return android_id, imei, oaid, mac
+
 def handle_transform_event(callback, imei, oaid, type):
-    # print(callback, imei, oaid, type)
     signature = 'wRZHhuS-UsgJh-KNr-mGHpYgoJjXKSXWE'
     url = '{0}&imei={1}&oaid={2}&event_type={3}&signature={4}'.\
         format(callback, imei, oaid, type, signature)
@@ -16,7 +34,6 @@ def handle_transform_event(callback, imei, oaid, type):
     try:
         resp = requests.get(url, timeout=3)
         json_data = resp.json()
-        # print json_data
         if json_data.get('code') == 0:
             return
         raise ValueError(json_data.get('msg'))
@@ -26,11 +43,12 @@ def handle_transform_event(callback, imei, oaid, type):
 
 def handle_activate_event(user: User):
     model = TransformEvent
-    # objs = ClickEvent.objects.filter(Q(android_id=user.android_id) | Q(imei=user.imei)
-    #                                  | Q(oaid=user.oaid) | Q(mac=user.mac)).all()
-    if user.imei == '':
-        return
-    objs = ClickEvent.objects.filter(imei=user.imei).all()
+    android_id, imei, oaid, mac = transform_blank_to_zero(user)
+    objs = ClickEvent.objects.filter(Q(android_id=android_id) | Q(imei=imei)
+                                     | Q(oaid=oaid) | Q(mac=mac)).all()
+    # if user.imei == '':
+    #     return
+    # objs = ClickEvent.objects.filter(imei=user.imei).all()
     if not objs:
         return
     object = model(transform='bytedance', action='activate', user_id=user.id, name=user.name)
@@ -40,11 +58,12 @@ def handle_activate_event(user: User):
 
 def handle_pay_event(user: User):
     model = TransformEvent
-    # objs = ClickEvent.objects.filter(Q(android_id=user.android_id) | Q(imei=user.imei)
-    #                                   | Q(oaid=user.oaid) | Q(mac=user.mac)).all()
-    if user.imei == '':
-        return
-    objs = ClickEvent.objects.filter(imei=user.imei).all()
+    android_id, imei, oaid, mac = transform_blank_to_zero(user)
+    objs = ClickEvent.objects.filter(Q(android_id=android_id) | Q(imei=imei)
+                                     | Q(oaid=oaid) | Q(mac=mac)).all()
+    # if user.imei == '':
+    #     return
+    # objs = ClickEvent.objects.filter(imei=user.imei).all()
     if not objs:
         return
     object = model(transform='bytedance', action='pay', user_id=user.id, name=user.name)
@@ -54,11 +73,12 @@ def handle_pay_event(user: User):
 
 def handle_twice_event(user: User):
     model = TransformEvent
-    # objs = ClickEvent.objects.filter(Q(android_id=user.android_id) | Q(imei=user.imei)
-    #                                   | Q(oaid=user.oaid) | Q(mac=user.mac)).all()
-    if user.imei == '':
-        return
-    objs = ClickEvent.objects.filter(imei=user.imei).all()
+    android_id, imei, oaid, mac = transform_blank_to_zero(user)
+    objs = ClickEvent.objects.filter(Q(android_id=android_id) | Q(imei=imei)
+                                     | Q(oaid=oaid) | Q(mac=mac)).all()
+    # if user.imei == '':
+    #     return
+    # objs = ClickEvent.objects.filter(imei=user.imei).all()
     if not objs:
         return
     object = model(transform='bytedance', action='twice', user_id=user.id, name=user.name)
