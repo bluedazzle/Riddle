edce34373271c327bf9c6288ef293acaf3121859
goodnighthy <hengyu1213@163.com>
2020-12-30 18:41:39 +0800

    add upload event

---
+++ event/management/commands/act_event.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+import datetime
+
+from django.core.management.base import BaseCommand
+
+from account.models import User
+from event.utils import upload_activate_event
+
+class Command(BaseCommand):
+    def event_init(self):
+        users = User.objects.filter(create_time__gte=datetime.datetime.now().date()).all()
+        for user in users:
+            upload_activate_event(user)
+
+    def handle(self, *args, **options):
+        self.event_init()
