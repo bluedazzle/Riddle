4abeef7ea0a4950350ee11bbfcd7fc9ef3430d9d
yaguang <shiyaguang19941023@gmail.com>
2021-01-22 14:13:28 +0800

    save

--- event/forms.py
+++ event/forms.py
@@ -14,6 +14,11 @@ class AdEventForm(forms.ModelForm):
     ad_type = forms.CharField(required=True, max_length=9999, error_messages=cash_messages)
     channel = forms.CharField(required=False, max_length=1000)
     extra = forms.CharField(required=False, max_length=1024)
+    ecpm = forms.FloatField(required=False, default=0)
+    ecpm_precision = forms.CharField(required=False, max_length=128, default=0)
+    ad_source_index = forms.IntegerField(required=False, default=0)
+    currency = forms.CharField(required=False, max_length=128, default=0)
+
 
     def save(self, commit=False):
         return super(AdEventForm, self).save(commit)
